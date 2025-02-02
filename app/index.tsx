7648090a39556d0efb44fb422e6285588a685617
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-02-02 01:12:31 -0800

    initial audio element development

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { useState } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
 
-export default function Index() {
+const audioSource =
+  'https://ssl-proxy.icastcenter.com/get.php?type=Icecast&server=199.180.72.2&port=9007&mount=/stream&data=mp3';
+
+export default function App() {
+  const player = useAudioPlayer(audioSource);
+  const [buttonText, setButtonText] = useState('Play Live Stream');
+
+  console.log(player.currentStatus);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Button
+        title={buttonText}
+        onPress={() => {
+          if (player.playing) {
+            player.pause();
+            setButtonText('Play Live Stream');
+          } else {
+            player.play();
+            setButtonText('Stream Loading');
+            const intervalId = setInterval(() => {
+              if (player.currentStatus.playing) {
+                setButtonText('Pause Stream');
+                clearInterval(intervalId); // Stop checking once the condition is met
+              }
+            }, 100); // Check every 100 ms
+          }
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+});
