6d1252686d52d6f7cbe190dde2c7ac1fefb60ae8
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-02-02 22:40:20 -0800

    rework audio to use expo-av package, add light/dark themeing

--- app/index.tsx
+++ app/index.tsx
@@ -1,43 +1,123 @@
-import { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { AudioPlayer, useAudioPlayer } from 'expo-audio';
+import { useState, useContext } from 'react';
+import { View, StyleSheet, Pressable, Text } from 'react-native';
+import { Audio } from 'expo-av';
+import { ThemeContext } from '@/context/ThemeContext';
+import { Octicons } from '@expo/vector-icons';
 
-const audioSource: string =
-  'https://ssl-proxy.icastcenter.com/get.php?type=Icecast&server=199.180.72.2&port=9007&mount=/stream&data=mp3';
+interface ThemeType {
+  text: string;
+  background: string;
+  icon: string;
+  button: string;
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  tertiary70: string;
+}
 
 export default function App() {
-  const player: AudioPlayer = useAudioPlayer(audioSource);
+  const audioSource: string =
+    'https://ssl-proxy.icastcenter.com/get.php?type=Icecast&server=199.180.72.2&port=9007&mount=/stream&data=mp3';
   const [buttonText, setButtonText] = useState<string>('Play Live Stream');
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+  const [audioPlayingStatus, setAudioPlayingStatus] = useState<boolean>(false);
+  //@ts-ignore
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+
+  const [sound, setSound] = useState<Audio.Sound>();
+
+  const avSource = {
+    uri: audioSource,
+  };
+
+  async function handleButtonClick() {
+    if (audioPlayingStatus) {
+      // console.log('handle function goes to stop');
+      await stopSound();
+    } else {
+      // console.log('handle function goes to start');
+      await playSound();
+    }
+  }
+
+  async function playSound() {
+    // console.log('Loading Sound');
+    setButtonDisabled(true);
+    setButtonText('Stream Loading');
+
+    const { sound } = await Audio.Sound.createAsync(avSource);
+    setSound(sound);
+
+    await sound.playAsync();
+
+    const intervalId = setInterval(async () => {
+      let audioStatus = await sound.getStatusAsync();
+      //@ts-ignore
+      if (audioStatus.isPlaying) {
+        setButtonText('Pause Stream');
+        // console.log('Playing Sound');
+        setButtonDisabled(false);
+        setAudioPlayingStatus(true);
+        clearInterval(intervalId); // Stop checking once the condition is met
+      }
+    }, 100); // Check every 100 ms
+  }
+  async function stopSound() {
+    //@ts-ignore
+    await sound.pauseAsync();
+    setButtonText('Play Live Stream');
+
+    // console.log('stopping Playback');
+  }
+
+  const styles = createStyles(theme, colorScheme);
 
   return (
     <View style={styles.container}>
-      <Button
-        title={buttonText}
+      <Pressable
+        onPress={() =>
+          setColorScheme(colorScheme === 'light' ? 'dark' : 'light')
+        }
+        style={{ marginLeft: 10 }}
+      >
+        <Octicons
+          name={colorScheme === 'dark' ? 'moon' : 'sun'}
+          size={36}
+          color={theme.text}
+          selectable={undefined}
+          style={{ width: 36 }}
+        />
+      </Pressable>
+      <Pressable
         onPress={() => {
-          if (player.playing) {
-            player.pause();
-            setButtonText('Play Live Stream');
-          } else {
-            player.play();
-            setButtonText('Stream Loading');
-            const intervalId = setInterval(() => {
-              if (player.currentStatus.playing) {
-                setButtonText('Pause Stream');
-                clearInterval(intervalId); // Stop checking once the condition is met
-              }
-            }, 100); // Check every 100 ms
-          }
+          handleButtonClick();
         }}
-      />
+        style={styles.audioButton}
+        disabled={buttonDisabled}
+      >
+        <Text style={styles.audioButtonText}>{buttonText}</Text>
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
-  },
-});
+function createStyles(theme: ThemeType, colorScheme: string) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: theme.background,
+      padding: 10,
+    },
+    audioButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    audioButtonText: {
+      fontSize: 18,
+      color: theme.primary,
+      fontWeight: 'bold',
+    },
+  });
+}
