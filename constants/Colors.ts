6d8706814dbd46dae992b8c36fcf280e6620dd04
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-02-02 20:24:04 -0800

    initial theme-ing work

---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+export const Colors = {
+  light: {
+    text: 'black',
+    background: 'white',
+    icon: 'black',
+    button: 'royalblue',
+    primary: '#ef6d00ff',
+    secondary: '#646464ff',
+    tertiary: '#192378ff',
+    tertiary70: '#192378b3',
+  },
+  dark: {
+    text: 'white',
+    background: 'black',
+    icon: 'red',
+    button: 'white',
+    primary: '#ef6d00ff',
+    secondary: '#646464ff',
+    tertiary: '#192378ff',
+    tertiary70: '#192378b3',
+  },
+};
